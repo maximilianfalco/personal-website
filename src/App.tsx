7c8f92cc84ff3ada91b699a8fa4aac51bffde929
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2025-02-08 00:57:19 +1100

    removed unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import './App.css'
 import { ReactLenis } from 'lenis/react'
 import Hero from './sections/Hero.tsx';
-import CurrentlyWorkingOn from './sections/CurrentlyWorkingOn.tsx';
-import PastProjects from './sections/PastProjects.tsx';
 
 function App() {
   return (
     <ReactLenis root>
       <div className='overflow-auto'>
         <Hero />
-        <div className='px-20 box-border mb-24 gap-10 flex flex-col'>
-          {/* <CurrentlyWorkingOn /> */}
-          {/* <PastProjects /> */}
-        </div>
       </div>
     </ReactLenis>
   )
