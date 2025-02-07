1bc76fa46625bac5e3fdd7e82838a2a2425c7926
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2025-02-08 01:10:54 +1100

    made dark as default theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export function useTheme() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || 'light');
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || 'dark');
 
   useEffect(() => {
     if (theme === "dark") {
